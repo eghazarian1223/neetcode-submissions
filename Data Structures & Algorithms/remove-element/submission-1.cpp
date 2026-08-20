bfd2a9dafd4ab143907b9a538d2f6311b1377d56
eghazarian1223 <ghaza034@umn.edu>
2026-08-19 23:16:23 -0700

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int non_val = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != val) {
+                nums[non_val] = nums[i];  
+                ++non_val;                
+            }
+        }
+        return non_val;
+    }
+};
