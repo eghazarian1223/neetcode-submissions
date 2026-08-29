53b972353044b0ee943d30ee9876ff68b83a502e
eghazarian1223 <ghaza034@umn.edu>
2026-08-29 02:22:34 -0400

    Add: intersection-of-two-arrays - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, 
+    vector<int>& nums2) {
+        // create empty array
+        vector<int> intersection;
+        set<int> unique;
+        // loop through nums 1 and nums 2
+        // separate loops to acct for diff sizes
+        for (int i = 0; i < nums1.size(); ++i) {
+            // elem in nums1 not in set
+            if (unique.find(nums1[i]) == unique.end()) {
+                unique.insert(nums1[i]);
+            }
+        }
+        for (int j = 0; j < nums2.size(); ++j) {
+            // elem in nums2 is in set
+            if (unique.find(nums2[j]) != unique.end()) {
+                intersection.push_back(nums2[j]);
+                unique.erase(nums2[j]);
+            }
+
+        }
+        return intersection;
+    }
+};
