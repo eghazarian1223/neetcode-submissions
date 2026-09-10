4e807f9a718fbdf3446cb55d6424c0c9026ae2ba
eghazarian1223 <ghaza034@umn.edu>
2026-09-10 00:58:47 -0400

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> output(nums.size());
+        vector<int> pref(nums.size());
+        vector<int> suff(nums.size());
+        // need to establish starting value boundaries
+        pref[0] = 1;
+        suff[nums.size() - 1] = 1;
+        // prefix
+        for (int i = 1; i < nums.size(); ++i) {
+            pref[i] = nums[i-1] * pref[i-1];     
+        }
+
+        // suffix
+        for (int i = nums.size() - 2; i >= 0; --i) {
+            suff[i] = nums[i+1] * suff[i+1];
+        }
+
+        // combine
+        for (int i = 0; i < nums.size(); ++i) {
+            output[i] = pref[i] * suff[i];
+        }
+
+        return output;
+    }
+};
